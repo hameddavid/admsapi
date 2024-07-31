b9aad6ae911729efbf149e73d886b8b78f1133e9
hameddavid <reganalyst@yahoo.com>
2024-07-31 15:24:30 +0100

    Update on the selection/category

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -58,40 +58,52 @@ class ApplicationController extends Controller
             if($request->jambScore >= $jamb_cutoff_   && $request->filled("jambScore")){
                 if( $request->filled('batchId')){
                     $all_applications = Application::where('session_id_FK', $config->_current_session_FK)
+                    ->join('t_applicants', 't_applications.last_updated_by', '=', 't_applicants.jamb_no')
                     ->where('id_of_screening_schedule', $request->batchId)
                     ->where('ume_score','>=', $request->jambScore )
                     ->where('app_category', 'UME')
+                    ->select('t_applications.*', 't_applicants.date_of_birth')
                     ->orderBy('first_choice_programme_FK', 'asc')->orderBy('avg_ume_pume_score', 'desc')->get();
                     return AllApplicationResource::collection($all_applications);
                 }
                 $all_applications = Application::where('session_id_FK', $config->_current_session_FK)
+                ->join('t_applicants', 't_applications.last_updated_by', '=', 't_applicants.jamb_no')
                 ->where('ume_score','>=', $request->jambScore )
                 ->where('app_category', 'UME')
+                ->select('t_applications.*', 't_applicants.date_of_birth')
                 ->orderBy('first_choice_programme_FK', 'asc')->orderBy('avg_ume_pume_score', 'desc')->get();
                 return AllApplicationResource::collection($all_applications);
             }
             elseif($request->jambScore < $jamb_cutoff_  && $request->filled("jambScore")){
                 $all_applications = Application::where('session_id_FK', $config->_current_session_FK)
+                ->join('t_applicants', 't_applications.last_updated_by', '=', 't_applicants.jamb_no')
                 ->where('ume_score','<', $jamb_cutoff_  )
                 ->where('app_category', 'UME')
+                ->select('t_applications.*', 't_applicants.date_of_birth')
                 ->orderBy('first_choice_programme_FK', 'asc')->orderBy('avg_ume_pume_score', 'desc')->get();
                 return AllApplicationResource::collection($all_applications);
             }else{
                 $all_applications = Application::where('session_id_FK', $config->_current_session_FK)
+                ->join('t_applicants', 't_applications.last_updated_by', '=', 't_applicants.jamb_no')
                 ->where('app_category', 'UME')
+                ->select('t_applications.*', 't_applicants.date_of_birth')
                 ->orderBy('first_choice_programme_FK', 'asc')->orderBy('avg_ume_pume_score', 'desc')->get();
                 return AllApplicationResource::collection($all_applications);
             }
         }
         elseif($request->category == 'DIRECT'){
             $all_applications = Application::where('session_id_FK', $config->_current_session_FK)
+            ->join('t_applicants', 't_applications.last_updated_by', '=', 't_applicants.jamb_no')
             ->where('app_category', 'DIRECT')
+            ->select('t_applications.*', 't_applicants.date_of_birth')
             ->orderBy('first_choice_programme_FK', 'asc')->orderBy('avg_ume_pume_score', 'desc')->get();
             return AllApplicationResource::collection($all_applications);
         }
         elseif($request->category == 'TRANSFER'){
             $all_applications = Application::where('session_id_FK', $config->_current_session_FK)
+            ->join('t_applicants', 't_applications.last_updated_by', '=', 't_applicants.jamb_no')
             ->where('app_category', 'TRANSFER')
+            ->select('t_applications.*', 't_applicants.date_of_birth')
             ->orderBy('first_choice_programme_FK', 'asc')->orderBy('avg_ume_pume_score', 'desc')->get();
             return AllApplicationResource::collection($all_applications);
         }
