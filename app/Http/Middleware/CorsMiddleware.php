bbb3059bacee0aa676bc68b6546539d9ff099397
hameddavid <reganalyst@yahoo.com>
2024-08-04 20:24:25 +0100

    Bulk admission

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -11,8 +11,7 @@ class CorsMiddleware
     public function handle(Request $request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin', 'https://reg.run.edu.ng') // Replace with specific origins if needed
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept, X-Requested-With');
+            ->header('Access-Control-Allow-Origin', '*') // Replace with specific origins if needed
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     }
 }
